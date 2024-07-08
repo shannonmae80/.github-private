cbf66b3b1886a8ea4c24c6c4075f3404158e5340
Shannon Mitchell <110545095+shannonmae80@users.noreply.github.com>
2024-07-08 13:16:50 -0400

    Initialize users/JohannesLampel/WorkingVersion

---
+++ src/ContributorLicenseAgreement.Core/Constants.cs
@@ -0,0 +1,18 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Microsoft License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace ContributorLicenseAgreement.Core
+{
+    public static class Constants
+    {
+        internal const string CheckName = "license/cla";
+        internal const string Check = "Check";
+        internal const string CheckSuccessTitle = "All CLA requirements met.";
+        internal const string CheckInProgressTitle = "Contributor License Agreement is not agreed yet.";
+        internal const string CheckSummary = "This check verifies that the author has agreed to a CLA with Microsoft.";
+        internal const string Agree = "agree";
+        internal const string Terminate = "terminate";
+    }
+}
---
+++ src/ContributorLicenseAgreement.Core/ContributorLicenseAgreementStartup.cs
@@ -0,0 +1,48 @@
+﻿namespace ContributorLicenseAgreement.Core
+{
+    using System.Diagnostics.CodeAnalysis;
+    using ContributorLicenseAgreement.Core.GitHubLinkClient;
+    using ContributorLicenseAgreement.Core.Handlers;
+    using ContributorLicenseAgreement.Core.Handlers.Helpers;
+    using GitOps.Apps.Abstractions;
+    using GitOps.Clients.Azure.BlobStorage;
+    using GitOps.Clients.GitHub.Configuration;
+    using GitOps.Common.Library.Extensions;
+    using GitOps.Primitives;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+
+    [ExcludeFromCodeCoverage]
+    public sealed class ContributorLicenseAgreementStartup : AppStartupBase
+    {
+        /// <summary>
+        /// This method is called when the app is initialized by the GitOps app server.
+        /// Here the app can register items that will be added to the app's dependency
+        /// injection container, just like a Startup.cs in a .NET web app.
+        /// </summary>
+        /// <param name="serviceCollection">Service collection.</param>
+        /// <param name="configuration">Configuration.</param>
+        public override void ConfigureServices(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            var azureBlobSettings = configuration.GetSection(nameof(AzureBlobSettings)).Get<AzureBlobSettings>();
+            serviceCollection.TryAddSingleton<IBlobStorage>(
+                p => new BlobStorage(
+                    azureBlobSettings.AccountName,
+                    azureBlobSettings.AccountKey,
+                    true));
+            var gitHubLinkSettings = configuration.GetSection(nameof(GitHubLinkSettings)).Get<GitHubLinkSettings>();
+            serviceCollection.TryAddSingleton<GitHubLinkRestClient>(p => new GitHubLinkRestClient(
+                gitHubLinkSettings.ApiUrl,
+                gitHubLinkSettings.ApiToken,
+                gitHubLinkSettings.ApiVersion));
+            serviceCollection.AddSingleton<PrimitiveCollection>();
+            serviceCollection.RegisterAad(configuration);
+            serviceCollection.AddSingleton<PullRequestHandler>();
+            serviceCollection.AddSingleton<IssueCommentHandler>();
+            serviceCollection.AddSingleton<GitHubHelper>();
+            serviceCollection.Configure<PlatformAppFlavorSettings>(
+                configuration.GetSection(nameof(PlatformAppFlavorSettings)));
+        }
+    }
+}
