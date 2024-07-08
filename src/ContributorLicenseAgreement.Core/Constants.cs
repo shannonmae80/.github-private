a316e8d6a50c01999d3e96b55ca2bd0041e2922d
Shannon Mitchell <110545095+shannonmae80@users.noreply.github.com>
2024-07-08 13:17:14 -0400

    Initialize DragosDanielBoia-patch-2

---
+++ src/ContributorLicenseAgreement.Core/Constants.cs
@@ -0,0 +1,19 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Microsoft License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace ContributorLicenseAgreement.Core
+{
+    public static class Constants
+    {
+        public const string CheckName = "license/cla";
+        public const string CheckSuccessTitle = "All CLA requirements met.";
+        public const string CheckSummary = "This check verifies that the author has agreed to a CLA with Microsoft.";
+        internal const string Check = "Check";
+        internal const string CheckInProgressTitle = "Contributor License Agreement is not agreed yet.";
+        internal const string Agree = "agree";
+        internal const string Terminate = "terminate";
+        internal const string Company = "company";
+    }
+}
