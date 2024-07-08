3b58e8f07dbb9dfd4016df6d7e9eac52a0ef9f24
Shannon Mitchell <110545095+shannonmae80@users.noreply.github.com>
2024-07-08 13:16:57 -0400

    Initialize users/JohannesLampel/logging2

---
+++ src/ContributorLicenseAgreement.Core/CLA.cs
@@ -0,0 +1,48 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Microsoft License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace ContributorLicenseAgreement.Core
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using ContributorLicenseAgreement.Core.Primitives.Data;
+    using GitOps.Abstractions;
+    using GitOps.Apps.Abstractions;
+    using GitOps.Apps.Abstractions.AppEventHandler;
+    using GitOps.Apps.Abstractions.Models;
+    using GitOps.Primitives;
+
+    public sealed class CLA : AppBase
+    {
+        private readonly PrimitiveCollection primitiveCollection;
+        private readonly AppEventHandlerOrchestrator appEventHandlerOrchestrator;
+
+        public CLA(
+            PrimitiveCollection primitiveCollection,
+            AppEventHandlerOrchestrator appEventHandlerOrchestrator)
+        {
+            this.primitiveCollection = primitiveCollection;
+            this.appEventHandlerOrchestrator = appEventHandlerOrchestrator;
+        }
+
+        public override string Id { get; protected set; } = nameof(ContributorLicenseAgreement);
+
+        public override async Task<AppOutput> Run(GitOpsPayload gitOpsPayload)
+        {
+            var appOutput = new AppOutput
+            {
+                Conclusion = Conclusion.Neutral
+            };
+
+            var primitives = (await primitiveCollection.GetOrgLevelFromConfigAsCodeRepos(gitOpsPayload))
+                .Where(p => p is ClaPrimitive)
+                .Cast<ClaPrimitive>();
+
+            await appEventHandlerOrchestrator.HandleEvent(gitOpsPayload, appOutput, primitives, Id);
+
+            return appOutput;
+        }
+    }
+}
