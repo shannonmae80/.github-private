04428d73d58312e70f3246fb35668aa8f456aac8
Shannon Mitchell <110545095+shannonmae80@users.noreply.github.com>
2024-07-08 13:17:28 -0400

    Initialize DragosDanielBoia-patch-13

---
+++ src/ContributorLicenseAgreement.Core/Handlers/Model/SignedCla.cs
@@ -0,0 +1,37 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Microsoft License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace ContributorLicenseAgreement.Core.Handlers.Model
+{
+    public class SignedCla
+    {
+        public string GitHubUser { get; set; }
+
+        public string Company { get; set; }
+
+        public string MsftMail { get; set; }
+
+        public bool Employee { get; set; }
+
+        public long Signed { get; set; }
+
+        public long? Expires { get; set; }
+
+        public bool CanSelfTerminate { get; set; }
+
+        public override string ToString()
+        {
+            var user = $"User: {GitHubUser}";
+            user = Company == null || Company.Equals(string.Empty) ? user : user + $", Company: {Company}";
+            return Employee ? $"{user}, eMail: {MsftMail}" : user;
+        }
+
+        public string GetParsableLog()
+        {
+            var company = Company == null || Company.Equals(string.Empty) ? " " : Company;
+            return $"{GitHubUser};{company};{MsftMail ?? " "}";
+        }
+    }
+}
